af887f70cbe0e6fa13fd88d1f8dc509dd893d82b
albertgran <albertgran@hotmail.com>
2022-05-29 23:43:38 +0200

    03

--- simulations/netbench/projects/accturbo/analysis/acc_accuracy_K/analyze.py
+++ simulations/netbench/projects/accturbo/analysis/acc_accuracy_K/analyze.py
@@ -14,8 +14,6 @@ def get_drops(input_throughput_file, output_throughput_file):
                 timestamp = timestamp/1000000000  # we convert it to seconds
                 packet_size_bit = float(row[1])
                 flow_id = int(row[2])
-
-                #if (timestamp > 6 and timestamp < 11) or (timestamp > 16 and timestamp < 21) or (timestamp > 26 and timestamp < 31) or (timestamp > 36 and timestamp < 41):
                 if flow_id < 5:
                     benign_input = benign_input + packet_size_bit
     csv_file.close()
@@ -29,8 +27,6 @@ def get_drops(input_throughput_file, output_throughput_file):
                 timestamp = timestamp/1000000000  # we convert it to seconds
                 packet_size_bit = float(row[1])
                 flow_id = int(row[2])
-
-                #if (timestamp > 6 and timestamp < 11) or (timestamp > 16 and timestamp < 21) or (timestamp > 26 and timestamp < 31) or (timestamp > 36 and timestamp < 41):
                 if flow_id < 5:
                     benign_output = benign_output + packet_size_bit
     csv_file.close()
@@ -53,8 +49,6 @@ def analyze_accuracy(input_throughput_file, output_throughput_file, results_file
                 timestamp = timestamp/1000000000  # we convert it to seconds
                 packet_size_bit = float(row[1])
                 flow_id = int(row[2])
-
-                #if (timestamp > 6 and timestamp < 11) or (timestamp > 16 and timestamp < 21) or (timestamp > 26 and timestamp < #31) or (timestamp > 36 and timestamp < 41):
                 if flow_id < 5:
                     benign_input = benign_input + packet_size_bit
     csv_file.close()
@@ -68,8 +62,6 @@ def analyze_accuracy(input_throughput_file, output_throughput_file, results_file
                 timestamp = timestamp/1000000000  # we convert it to seconds
                 packet_size_bit = float(row[1])
                 flow_id = int(row[2])
-
-                #if (timestamp > 6 and timestamp < 11) or (timestamp > 16 and timestamp < 21) or (timestamp > 26 and timestamp < #31) or (timestamp > 36 and timestamp < 41):
                 if flow_id < 5:
                     benign_output = benign_output + packet_size_bit
     csv_file.close()
@@ -78,7 +70,7 @@ def analyze_accuracy(input_throughput_file, output_throughput_file, results_file
     benign_drops = (benign_input - benign_output)/benign_input * 100
 
     # Save results
-    if scheduler == "K1":
+    if scheduler == "K10":
         output_file = open(results_file, 'w')
         output_file.write("#K, % Benign Drops, FIFO Drops, ACCTurbo Drops\n")
         output_file.write("{},{},{},{}\n".format(scheduler, benign_drops, fifo_drops, accturbo_drops))
@@ -97,7 +89,7 @@ if __name__ == "__main__":
     accturbo_drops =  get_drops('temp/accturbo/acc_accuracy_K/accturbo/aggregate_input_throughput.csv.log',
                              'temp/accturbo/acc_accuracy_K/accturbo/aggregate_output_throughput.csv.log')
     # Call analysis functions
-    for scheduler in ["K1", "K25", "K50", "K100", "K250", "K500", "K1000"]:
+    for scheduler in ["K10", "K25", "K50", "K100", "K250", "K500", "K1000", "K1500", "K2000"]:
         analyze_accuracy('temp/accturbo/acc_accuracy_K/{}/aggregate_input_throughput.csv.log'.format(scheduler),
                          'temp/accturbo/acc_accuracy_K/{}/aggregate_output_throughput.csv.log'.format(scheduler),
                          'projects/accturbo/analysis/acc_accuracy_K/benign_drops.dat', scheduler, fifo_drops, accturbo_drops)
